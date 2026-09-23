68eea8ccf81576e12e41c29dbfd8bbcf9afac8db
abenezer7813 <abenimom1994@gmail.com>
2026-09-23 10:44:18 +0300

    feat: add loading and error states to menu fetch

---
+++ src/components/loading-sppiner/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LoadingSpinner() {
+  return (
+    <div>Loading</div>
+  )
+}
+
+export default LoadingSpinner
--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -1,13 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React, { use, useEffect, useState } from 'react'
 import styles from './Menu.module.css'
 import MenuCard from '../../components/menu-card/MenuCard'
 import Categories from '../../components/catagories/Categories'
 import Search from '../../components/search/Search'
+import LoadingSpinner from '../../components/loading-sppiner/LoadingSpinner'
+import Error from '../../components/error/Error'
 
 function Menu() {
     const [dishes, setDishes] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
     const [selectedCategory, setSelectedCategory] = useState('All')
-    const [searchTerm,setSearchTerm]=useState('')
+    const [searchTerm, setSearchTerm] = useState('')
     const categories = [
         "All",
         "Traditional Stews & Wat",
@@ -16,7 +20,7 @@ function Menu() {
         "Fasting & Vegan / Tsom",
         "Beverages & Tej"
     ]
-    
+
 
     useEffect(() => {
 
@@ -25,8 +29,13 @@ function Menu() {
             .then(json => {
                 console.log('raw response:', json)
                 setDishes(json.data)
+                setLoading(false)
             })
-            .catch(err => console.log('fetch error:', err.message))
+            .catch(err => {
+                console.log('fetch error:', err.message);
+                setError(err.message)
+                setLoading(false)
+            })
 
 
 
@@ -35,11 +44,18 @@ function Menu() {
     const filteredDishes = selectedCategory === 'All'
         ? dishes
         : dishes.filter((d) => d.category === selectedCategory)
-    const search=filteredDishes.filter(d => d.nameEn.toLowerCase().includes(searchTerm.toLowerCase()))
+    const search = filteredDishes.filter(d => d.nameEn.toLowerCase().includes(searchTerm.toLowerCase()))
+if(loading){
+    return <LoadingSpinner/>
+}
+     if(error){
+        return <Error error={error}/>
+     }
+
     return (
         <div className={styles.menuPage}>
             <div className={styles.search}>
-                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
             </div>
             <div className={styles.categories}>
                 <Categories categories={categories} current={selectedCategory} onSelect={setSelectedCategory} />
